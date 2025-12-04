03446f3c72e10ad7e943df373809a2cdc793d309
kungjin <vudchsdl123@gmail.com>
2025-12-04 17:31:28 +0900

    feat: test Carpick-Zone frontend Simple Test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CarDetailPage from './pages/CarDetailPage.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        메인페이지 수정 테스트
-      </div>
-    </>
-  )
+  return <CarDetailPage />;
 }
-
 export default App
