4636a6f41c2b81fb854c42fa750e4fe29869e270
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-12-10 16:06:05 +0900

    하단버튼 및 서브메뉴 수정

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,13 +1,14 @@
 
 const Footer = () => {
+    // 활성화 시 클래스명 'active' 추가
     return (
         <div id="footer">
             <div className="inner">
                 <div className="btns">
-                    <button>Footer1</button>
-                    <button>Footer2</button>
-                    <button>Footer3</button>
-                    <button>Footer4</button>
+                    <button type="button" className={`btn btn_ai ${location.pathname.includes("aipick") ? "active" : ""}`}><span>AI PICK</span></button>
+                    <button type="button" className={`btn btn_car ${location.pathname.includes("day") ? "active" : ""}`}><span>단기렌트</span></button>
+                    <button type="button" className={`btn btn_cal ${location.pathname.includes("month") ? "active" : ""}`}><span>장기렌트</span></button>
+                    <button type="button" className={`btn btn_my ${location.pathname.includes("my") ? "active" : ""}`}><span>마이페이지</span></button>
                 </div>
             </div>
         </div>
