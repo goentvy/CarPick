0e979e08807d404bbe9da7f7340251fc639fe80d
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-12-18 16:54:09 +0900

    feat.관리자페이지 오류 수정 및 인트로 경로 수정

---
+++ backend/src/main/java/com/carpick/domain/Intro/controller/RecommendController.java
@@ -0,0 +1,33 @@
+package com.carpick.domain.Intro.controller;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import com.carpick.domain.Intro.service.RecommendService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class RecommendController {
+
+    private final RecommendService recommendService;
+
+    @PostMapping("/recommend-cars")
+    public Map<String, Object> recommendCars(@RequestBody RecommendRequest request) {
+
+        Map<String, String> segment = recommendService.getRecommendedSegment(request.getOptions());
+
+        return Map.of(
+                "recommendedSegment", segment
+        );
+    }
+
+    @Data
+    public static class RecommendRequest {
+        private List<String> options;  // 체크박스 데이터 배열
+    }
+}
