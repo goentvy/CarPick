20aca6670823a7cea3d1b6c0db39c5f6880e23e8
gxnny19 <wkdrudrjs010@gmail.com>
2025-12-19 16:26:37 +0900

    refactor: 유저 공지사항 페이지 640px로 수정

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://3.236.8.244:8080/',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
