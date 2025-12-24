482dfab20d3b662a88fb9fbc77084080201cc747
NAYUJIN <won10ve8953@gmail.com>
2025-12-24 14:50:35 +0900

    fix users db수정

--- backend/src/main/resources/db/migration/users.sql
+++ backend/src/main/resources/db/migration/users.sql
@@ -13,13 +13,13 @@ CREATE TABLE users (
 
     -- 로그인 식별
                        email VARCHAR(255) NOT NULL UNIQUE,
-                       password_hash VARCHAR(255) NULL,
-
-                       provider ENUM('LOCAL','KAKAO','NAVER') NOT NULL,
-                       provider_id VARCHAR(255) NULL,
+                       PASSWORD VARCHAR(255) NULL,
+    -- 소셜 계정
+                       provider ENUM('LOCAL','KAKAO','NAVER') NOT NULL UNIQUE,
+                       provider_id VARCHAR(255) NULL UNIQUE,
 
     -- 개인정보 (선택)
-                       name VARCHAR(50) NULL,
+                       NAME VARCHAR(50) NULL,
                        phone VARCHAR(20) NULL,
                        birth DATE NULL,
                        gender ENUM('M','F') NULL,
@@ -33,9 +33,10 @@ CREATE TABLE users (
                            ON UPDATE CURRENT_TIMESTAMP,
                        deleted_at DATETIME NULL,
 
-    -- 소셜 계정 중복 방지
-                       UNIQUE KEY uk_social (provider, provider_id)
+    -- 토큰
+                       accesstoken VARCHAR(500)
 );
+
 
 
 
