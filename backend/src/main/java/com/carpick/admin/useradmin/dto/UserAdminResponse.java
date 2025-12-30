436ed81c670fb8dd254ade2553525377d37f319d
SimSBgit <sbct47@gmail.com>
2025-12-30 15:21:57 +0900

    feat 고객관리페이지 구현완료

--- backend/src/main/java/com/carpick/admin/useradmin/dto/UserAdminResponse.java
+++ backend/src/main/java/com/carpick/admin/useradmin/dto/UserAdminResponse.java
@@ -1,24 +1,17 @@
 package com.carpick.admin.useradmin.dto;
 
-import com.carpick.admin.useradmin.entity.UserAdmin;
-import lombok.Getter;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 public class UserAdminResponse {
 
-    private final Long userId;
-    private final String email;
-    private final String name;
-    private final String phone;
-    private final LocalDateTime createdAt;
-
-    public UserAdminResponse(UserAdmin user) {
-        this.userId = user.getUserId();
-        this.email = user.getEmail();
-        this.name = user.getName();
-        this.phone = user.getPhone();
-        this.createdAt = user.getCreatedAt();
-    }
+    private Long id;
+    private String email;
+    private String name;
+    private String phone;
+    private String membershipGrade;
+    private Integer marketingAgree;
+    private LocalDateTime createdAt;
 }
